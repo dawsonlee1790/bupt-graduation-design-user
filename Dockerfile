52825b50fc80edc608839ad013d9496506eadbfa
dawsonlee1790 <dawsonlee1790@outlook.com>
2019-05-07 12:22:31 +0800

    解决开发环境和部署环境资源配置问题

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM java:8
 MAINTAINER bupt.dawsonlee1790
-ADD build/libs/bupt-graduation-design-user-0.0.1-SNAPSHOT.jar app.jar
+ADD build/libs/bupt-graduation-design-user-1.0.jar app.jar
 EXPOSE 8080
 ENTRYPOINT java -jar app.jar
--- src/main/kotlin/bupt/dawsonlee1790/usermicroservice/UserMicroserviceApplication.kt
+++ src/main/kotlin/bupt/dawsonlee1790/usermicroservice/UserMicroserviceApplication.kt
@@ -9,66 +9,66 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Profile
 
 @SpringBootApplication
 class UserMicroserviceApplication {
 
-//    @Bean
-//    fun init(userRepository: UserRepository, groupRepository: GroupRepository) = CommandLineRunner {
-//        val passwordToken = SHA256().getSHA256StrJava("123456789")
-//        val user1 = User(
-//                name = "Dawson",
-//                password = passwordToken
-//        )
-//        val user2 = User(
-//                name = "Mary",
-//                password = passwordToken
-//        )
-//        val user3 = User(
-//                name = "Jack",
-//                password = passwordToken
-//        )
-//        val user4 = User(
-//                name = "Lina",
-//                password = passwordToken
-//        )
-//        val user5 = User(
-//                name = "Doinb",
-//                password = passwordToken
-//        )
-//        val group1 = Group(
-//                name = "计划人",
-//                description = "制定生产批次计划"
-//        )
-//        val group2 = Group(
-//                name = "研究员",
-//                description = "制定SOP流程"
-//        )
-//        val group3 = Group(
-//                name = "叉车工",
-//                description = "执行生产指令"
-//        )
-//        val group4 = Group(
-//                name = "车间管理员",
-//                description = "执行生产指令"
-//        )
-//        val group5 = Group(
-//                name = "生产负责人",
-//                description = "对一个批次的生产负责"
-//        )
-//        group1.userList.add(user1)
-//        group2.userList.add(user2)
-//        group3.userList.add(user3)
-//        group4.userList.add(user4)
-//        group5.userList.add(user5)
-//        groupRepository.save(group1)
-//        groupRepository.save(group2)
-//        groupRepository.save(group3)
-//        groupRepository.save(group4)
-//        groupRepository.save(group5)
-//    }
-
-
+    @Bean
+    @Profile("dev")
+    fun init(userRepository: UserRepository, groupRepository: GroupRepository) = CommandLineRunner {
+        val passwordToken = SHA256().getSHA256StrJava("123456789")
+        val user1 = User(
+                name = "Dawson",
+                password = passwordToken
+        )
+        val user2 = User(
+                name = "Mary",
+                password = passwordToken
+        )
+        val user3 = User(
+                name = "Jack",
+                password = passwordToken
+        )
+        val user4 = User(
+                name = "Lina",
+                password = passwordToken
+        )
+        val user5 = User(
+                name = "Doinb",
+                password = passwordToken
+        )
+        val group1 = Group(
+                name = "计划人",
+                description = "制定生产批次计划"
+        )
+        val group2 = Group(
+                name = "研究员",
+                description = "制定SOP流程"
+        )
+        val group3 = Group(
+                name = "叉车工",
+                description = "执行生产指令"
+        )
+        val group4 = Group(
+                name = "车间管理员",
+                description = "执行生产指令"
+        )
+        val group5 = Group(
+                name = "生产负责人",
+                description = "对一个批次的生产负责"
+        )
+        group1.userList.add(user1)
+        group2.userList.add(user2)
+        group3.userList.add(user3)
+        group4.userList.add(user4)
+        group5.userList.add(user5)
+        groupRepository.save(group1)
+        groupRepository.save(group2)
+        groupRepository.save(group3)
+        groupRepository.save(group4)
+        groupRepository.save(group5)
+    }
 }
 
 
