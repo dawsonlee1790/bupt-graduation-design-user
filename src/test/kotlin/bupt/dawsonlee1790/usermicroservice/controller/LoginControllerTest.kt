8e31201ea1847d2655256646db3bb946d03209e4
dawsonlee1790 <dawsonlee1790@outlook.com>
2019-05-07 23:24:24 +0800

    添加注册测试用例

--- src/test/kotlin/bupt/dawsonlee1790/usermicroservice/controller/LoginControllerTest.kt
+++ src/test/kotlin/bupt/dawsonlee1790/usermicroservice/controller/LoginControllerTest.kt
@@ -1,6 +1,7 @@
 package bupt.dawsonlee1790.usermicroservice.controller
 
 import bupt.dawsonlee1790.usermicroservice.UserMicroserviceApplication
+import org.intellij.lang.annotations.Language
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -13,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.context.WebApplicationContext
 
 @RunWith(SpringRunner::class)
@@ -40,6 +42,16 @@ class LoginControllerTest {
         ).andReturn()
     }
 
+    private fun put(url: String, content: String): MvcResult {
+        return mockMvc.perform(
+                MockMvcRequestBuilders
+                        .put(url)
+                        .content(content)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn()
+    }
+
+
     @Test
     fun loginSuccessful() {
         val url = "$baseUrl/login"
@@ -62,6 +74,19 @@ class LoginControllerTest {
         val contextJson = "{\n  \"name\": \"Dawson\",\n  \"passwordToken\": \"18\"\n}"
         val result = post(url, contextJson)
         Assert.assertNotEquals(200, result.response.status)
+    }
+
+    /**
+     * 其实因为dev环境每次都会重新创建数据库所以在dev环境测试是不需要Transactional回滚的
+     * 但product环境进行是需要Transactional回滚的，所以添加了
+     */
+    @Test
+    @Transactional
+    fun registerSuccessful() {
+        val url = "$baseUrl/register"
+        @Language("JSON") val contextJson = "{\n  \"name\": \"Uzl\",\n  \"password\": \"123456789\",\n  \"roleList\": [\n    \"Planner\"\n  ]\n}"
+        val result = put(url, contextJson)
+        Assert.assertEquals(200, result.response.status)
     }
 
 }
