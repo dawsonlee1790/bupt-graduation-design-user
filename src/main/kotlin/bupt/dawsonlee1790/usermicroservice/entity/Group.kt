d17cb0ef000a1edd6882ff373b78f76f7030dcc4
dawsonlee1790 <dawsonlee1790@outlook.com>
2019-05-07 17:38:37 +0800

    通过隐藏构造函数和使用静态函数实现对构造细节进行隐藏，体现面向对象编程的封装特性

--- src/main/kotlin/bupt/dawsonlee1790/usermicroservice/entity/Group.kt
+++ src/main/kotlin/bupt/dawsonlee1790/usermicroservice/entity/Group.kt
@@ -1,10 +1,11 @@
 package bupt.dawsonlee1790.usermicroservice.entity
 
+import bupt.dawsonlee1790.usermicroservice.enums.Role
 import javax.persistence.*
 
 @Entity
 @Table(name = "groups")
-class Group(
+class Group private constructor(
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         var id: Long? = null,
@@ -14,5 +15,15 @@ class Group(
         @JoinTable(name = "user_group",
                 joinColumns = [JoinColumn(name = "group_id", referencedColumnName = "id")],
                 inverseJoinColumns = [JoinColumn(name = "user_id", referencedColumnName = "id")])
-        var userList: MutableList<User> = ArrayList()
-)
+        var userList: MutableList<User>
+) {
+    companion object {
+        fun create(role: Role, userList: List<User> = ArrayList()): Group {
+            return Group(
+                    name = role.name,
+                    description = role.value,
+                    userList = userList.toMutableList()
+            )
+        }
+    }
+}
--- src/main/kotlin/bupt/dawsonlee1790/usermicroservice/entity/User.kt
+++ src/main/kotlin/bupt/dawsonlee1790/usermicroservice/entity/User.kt
@@ -6,7 +6,7 @@ import javax.persistence.*
 
 @Entity
 @Table(name = "users")
-class User(
+class User private constructor(
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         var id: Long? = null,
@@ -16,8 +16,19 @@ class User(
         @JoinTable(name = "user_group",
                 joinColumns = [JoinColumn(name = "user_id", referencedColumnName = "id")],
                 inverseJoinColumns = [JoinColumn(name = "group_id", referencedColumnName = "id")])
-        var groupList: MutableList<Group> = ArrayList()
+        var groupList: MutableList<Group>
 ) {
+
+    companion object {
+        fun create(name: String, passwordToken: String, groupList: List<Group> = ArrayList()): User {
+            val encryptedToken = SHA256().getSHA256StrJava(passwordToken)
+            return User(
+                    name = name,
+                    password = encryptedToken,
+                    groupList = groupList.toMutableList()
+            )
+        }
+    }
 
     fun verifyPassword(passwordToken: String): Boolean {
         val encryptedToken = SHA256().getSHA256StrJava(passwordToken)
